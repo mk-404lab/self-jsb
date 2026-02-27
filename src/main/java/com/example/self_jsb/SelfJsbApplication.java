213cb9f13f3aad82b56108412387f1e1f5c1be18
mk-404lab <alsgur9984@naver.com>
2026-02-27 17:33:21 +0900

    init

---
+++ src/main/java/com/example/self_jsb/SelfJsbApplication.java
@@ -0,0 +1,13 @@
+package com.example.self_jsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfJsbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfJsbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/self_jsb/SelfJsbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.self_jsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfJsbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
