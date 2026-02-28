d51ad372e1e6094905c524143e8071de6d5a482d
mk-404lab <alsgur9984@naver.com>
2026-03-01 01:52:27 +0900

    feat: 29강 - Transactional(readOnly=true) 설정으로 최적화

--- src/main/java/com/back/self_jsb/global/BaseInitData.java
+++ src/main/java/com/back/self_jsb/global/BaseInitData.java
@@ -2,13 +2,21 @@ package com.back.self_jsb.global;
 
 import com.back.self_jsb.post.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.transaction.annotation.Transactional;
 
 @Configuration
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class BaseInitData {
+
+    @Autowired
+    @Lazy
+    private BaseInitData self;
 
     private final PostService postService;
 
@@ -16,37 +24,20 @@ public class BaseInitData {
     ApplicationRunner initDataRunner() {
         return args -> {
 
-            work1();
-            work2();
+            self.work1();
+            self.work2();
         };
     }
 
-
-    /*
-    트랜잭션 이해하기 위한 예제
-    상황: 만약 어떤 기능을 테스트 데이터가 2개라고 가정하고 개발을 진행했다면?
-
-    첫 번째 write는 실행 -> if(true)에 의해 예외 발생 -> 두 번째 write는 실행되지 않음
-
-    두 개의 write가 모두 실행되거나 모두 실패하도록 만들기 위해 트랜잭션을 설정하는 것
-     */
-
+    @Transactional
     void work1(){
-        try {
-            if (postService.count() > 0) {
-                return;
-            }
 
-            postService.write("제목1", "내용1");
-
-            if (true) {
-                throw new RuntimeException("테스트 예외");
-            }
-
-            postService.write("제목2", "내용2");
-        } catch (Exception e) {
-            System.out.println("예외 발생 : " +e.getMessage());
+        if (postService.count() > 0) {
+            return;
         }
+
+        postService.write("제목1", "내용1");
+        postService.write("제목2", "내용2");
 
     }
 
