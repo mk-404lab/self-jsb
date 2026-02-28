b474c35b9d822cd0312a5b51140d91225edabaf9
mk-404lab <alsgur9984@naver.com>
2026-03-01 02:42:47 +0900

    feat: 31~32강 - Auditing, Listenter 기능을 추가해서 등록날짜와 수정날짜를 자동으로 갱신

--- src/main/java/com/back/self_jsb/post/entity/Post.java
+++ src/main/java/com/back/self_jsb/post/entity/Post.java
@@ -4,6 +4,9 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -11,6 +14,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor      // 기본 생성자를 자동으로 만들어주는 롬복 어노테이션
 @Setter                 // 롬복의 Setter, Getter 어노테이션을 추가하면 setTitle, getTitle과 같은 메서드를
 @Getter                 // 직접 구현할 필요가 없다.
+@EntityListeners(AuditingEntityListener.class)
 public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,7 +26,9 @@ public class Post {
     @Column(columnDefinition = "TEXT", nullable = false)
     private String content;
 
+    @CreatedDate
     private LocalDateTime createDate;
+    @LastModifiedDate
     private LocalDateTime modifyDate;
 
     public Post(String title, String content){
--- src/main/java/com/back/self_jsb/post/service/PostService.java
+++ src/main/java/com/back/self_jsb/post/service/PostService.java
@@ -4,6 +4,7 @@ import com.back.self_jsb.post.entity.Post;
 import com.back.self_jsb.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -26,9 +27,10 @@ public class PostService {
         return postRepository.save(post);
     }
 
-    public Post modify(Post post, String title, String content){
-        post.setTitle(title);
-        post.setContent(content);
+    @Transactional
+    public Post modify(Post post, String newTitle, String newContent){
+        post.setTitle(newTitle);
+        post.setContent(newContent);
         return postRepository.save(post);
     }
 
