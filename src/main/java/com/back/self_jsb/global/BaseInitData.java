67424f79dece29580ba8fc1f333e0e2f3cd41df5
mk-404lab <alsgur9984@naver.com>
2026-03-01 00:28:20 +0900

    feat: 24강 - 트랜잭션이 없을 때 예외가 발생하는 경우

--- src/main/java/com/back/self_jsb/global/BaseInitData.java
+++ src/main/java/com/back/self_jsb/global/BaseInitData.java
@@ -21,13 +21,33 @@ public class BaseInitData {
         };
     }
 
+
+    /*
+    트랜잭션 이해하기 위한 예제
+    상황: 만약 어떤 기능을 테스트 데이터가 2개라고 가정하고 개발을 진행했다면?
+
+    첫 번째 write는 실행 -> if(true)에 의해 예외 발생 -> 두 번째 write는 실행되지 않음
+
+    두 개의 write가 모두 실행되거나 모두 실패하도록 만들기 위해 트랜잭션을 설정하는 것
+     */
+
     void work1(){
-        if(postService.count() > 0){
-            return;
+        try {
+            if (postService.count() > 0) {
+                return;
+            }
+
+            postService.write("제목1", "내용1");
+
+            if (true) {
+                throw new RuntimeException("테스트 예외");
+            }
+
+            postService.write("제목2", "내용2");
+        } catch (Exception e) {
+            System.out.println("예외 발생 : " +e.getMessage());
         }
 
-        postService.write("제목1", "내용1");
-        postService.write("제목2", "내용2");
     }
 
     void work2() {
