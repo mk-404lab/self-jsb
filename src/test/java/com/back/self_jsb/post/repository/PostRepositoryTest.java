e8077e3c7f8563a33eb1615b3a640efdbb773eef
mk-404lab <alsgur9984@naver.com>
2026-03-02 21:47:35 +0900

    feat: 35~36강 - PostRepositoryTest 추가

---
+++ src/test/java/com/back/self_jsb/post/repository/PostRepositoryTest.java
@@ -0,0 +1,24 @@
+package com.back.self_jsb.post.repository;
+
+import com.back.self_jsb.post.entity.Post;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class PostRepositoryTest {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Test
+    void t1(){
+        Post post = postRepository.findById(2).get();
+
+        assertThat(post.getId()).isEqualTo(2);
+        assertThat(post.getTitle()).isEqualTo("제목2");
+        assertThat(post.getContent()).isEqualTo("내용2");
+    }
+}
